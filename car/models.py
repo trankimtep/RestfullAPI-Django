4d2ecdd76658386c237d92690b651d5de61957f1
TrieuXuanHung <trieuhung0202@gmail.com>
2022-05-26 22:24:28 +0700

    deployed

---
+++ car/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Car(models.Model):
+    name = models.CharField(max_length=50)
+    color = models.CharField(max_length=20)
+    brand = models.CharField(max_length=20)
+
